1b02d54c05f02af3838e06d4767d6e806b571f4d
unknown <200958@jnu.ac.kr>
2022-06-23 14:37:38 +0900

    DOM을 이용하여 데이터 불러오기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log("이것은 JS");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+
+    console.log(req);
+}
