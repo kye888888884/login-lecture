10901137c1cb9ec9f9f82fc7efb65875004999c8
kyh888888884 <k37237280@gmail.com>
2022-06-25 16:36:28 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,11 @@ function login() {
         pw: pw.value,
     };
 
-    console.log(req);
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
 }
